01d42b422e9ee22674e62e6e658a70268cc2b13d
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-04 14:39:16 +0900

    Title: 합과 차의 나눗셈, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231204/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        double sum = a + b;
+        double diff = a - b;
+        
+        double result = sum / diff;
+
+        System.out.printf("%.2f", result);
+    }
+}
