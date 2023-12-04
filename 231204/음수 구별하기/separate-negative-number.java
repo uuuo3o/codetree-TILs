6a8cccf0e42f7cfcc5573749af2b9df0b839f8e9
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-04 14:48:48 +0900

    Title: 음수 구별하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231204/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println(a);
+        if (a < 0) {
+            System.out.println("minus");
+        }
+    }
+}
