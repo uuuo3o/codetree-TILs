6cb73f20818bb3aa3f8affbf00e91b20386c278e
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-04 13:23:08 +0900

    Title: 실수 입력받아 계산, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231204/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+
+        System.out.printf("%.2f", (a + b));
+    }
+}
