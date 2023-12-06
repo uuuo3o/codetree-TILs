baae4870c5609afec26cfc73bbdd5b5063464714
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-06 14:47:33 +0900

    Title: 비교 연산, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231206/비교 연산/comparison-operator.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a >= b) System.out.println(1);
+        else System.out.println(0);
+
+        if (a > b) System.out.println(1);
+        else System.out.println(0);
+
+        if (b >= a) System.out.println(1);
+        else System.out.println(0);
+
+        if (b > a) System.out.println(1);
+        else System.out.println(0);
+
+        if (a == b) System.out.println(1);
+        else System.out.println(0);
+
+        if (a != b) System.out.println(1);
+        else System.out.println(0);
+    }
+}
