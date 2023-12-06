535179ce569129715ddc423dac26d633a8a6bf25
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-06 14:43:07 +0900

    Title: 특정 조건 두 정수 비교, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231206/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a < b) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.print(0 + " ");
+        }
+
+        if (a == b) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
