14bd5ab02f2588b905ba9f529d1a181daab58af8
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-06 14:54:08 +0900

    Title: 점수 비교, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231206/점수 비교/score-comparison.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int mathA = Integer.parseInt(st.nextToken());
+        int mathB = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int engA = Integer.parseInt(st.nextToken());
+        int engB = Integer.parseInt(st.nextToken());
+
+        System.out.println(mathA > mathB && engA > engB ? 1 : 0);
+    }
+}
