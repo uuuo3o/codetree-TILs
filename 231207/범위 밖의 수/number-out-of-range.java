af068d99cb2647b5b6bf8bf52da5f15a22f5807f
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-07 15:45:12 +0900

    Title: 범위 밖의 수, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231207/범위 밖의 수/number-out-of-range.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println((a < 10 || a > 20) ? "yes" : "no");
+    }
+}
