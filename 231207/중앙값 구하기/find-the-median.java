b20cf6d5d3e5a5903d9b2a730935126842835a42
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-07 17:04:58 +0900

    Title: 중앙값 구하기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231207/중앙값 구하기/find-the-median.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[3];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
