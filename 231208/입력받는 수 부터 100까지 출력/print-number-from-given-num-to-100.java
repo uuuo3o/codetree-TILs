fc64a4a07ea8f7a98e97130ab517e6e60bb95b5f
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-08 22:46:44 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231208/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i < 101; i++) {
+            bw.write(i + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
