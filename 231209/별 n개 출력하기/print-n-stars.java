1c164608ab34ee1ff16c39157ac98c1492c69b2a
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-09 09:47:03 +0900

    Title: 별 n개 출력하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231209/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int i = 1;
+
+        while (i <= n) {
+            bw.write("*\n");
+            i++;
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
