09d1c0041d2280bfd371a888f7969ff630e7206a
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-09 09:41:27 +0900

    Title: b부터 a까지 감소, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231209/b부터 a까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for (int i = b; i >= a; i--) {
+            bw.write(i + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
