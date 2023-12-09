92d1b01a9fb98604252f03f9dd320d757e03e6e7
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-09 09:48:27 +0900

    Title: 증가시키며 출력하기 3, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231209/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int i = 10;
+
+        while (i <= 26) {
+            bw.write(i + " ");
+            i += 2;
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
