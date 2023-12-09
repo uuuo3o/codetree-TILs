65577d007cb99fedda1148b26a6b7d89691c396d
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-09 09:40:22 +0900

    Title: 특정 숫자 도달하기 2, Time: 77ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231209/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = n; i > 0; i--) {
+            bw.write(i + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
