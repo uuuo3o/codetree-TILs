7517b9689f525179a0ea72eb88758bf1da13c646
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-11 11:00:37 +0900

    Title: 2 또는 3의 배수, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231211/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 2 == 0 || i % 3 == 0) {
+                bw.write(1 + " ");
+            } else {
+                bw.write(0 + " ");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
