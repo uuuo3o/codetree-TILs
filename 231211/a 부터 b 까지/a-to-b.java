fb6991cddfd50c41d4a7ae9461d6e94ffa7746aa
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-11 11:06:42 +0900

    Title: a 부터 b 까지, Time: 76ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231211/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for (int i = a; i < b + 1; i++) {
+            if (i % 2 == 0) {
+                bw.write(i + " ");
+                i += 2;
+            } else {
+                bw.write(i + " ");
+                i *= 2;
+                i--;
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
