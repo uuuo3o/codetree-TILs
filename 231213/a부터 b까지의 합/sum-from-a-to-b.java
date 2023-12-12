b67abeff157f023b6efe18b0d4a306c61badc3bd
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-13 00:22:40 +0900

    Title: a부터 b까지의 합, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231213/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            sum += i;
+        }
+
+        bw.write(sum + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
