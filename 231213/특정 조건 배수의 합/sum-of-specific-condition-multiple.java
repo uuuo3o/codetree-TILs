ed3a53d80d703502f483c78253962e81ed764d46
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-13 00:31:52 +0900

    Title: 특정 조건 배수의 합, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231213/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                if (i % 5 == 0) {
+                    sum += i;
+                }
+            }
+        } else {
+            for (int i = b; i <= a; i++) {
+                if (i % 5 == 0) {
+                    sum += i;
+                }
+            }
+        }
+        
+        bw.write(sum + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
