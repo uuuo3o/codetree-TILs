a0b37b94125a31fdbd57efb87c3ef93573676f42
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-18 23:01:45 +0900

    Title: a의 b승, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231218/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int prod = 1;
+        for (int i = 0; i < b; i++) {
+
+            prod *= a;
+        }
+
+        bw.write(prod + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
