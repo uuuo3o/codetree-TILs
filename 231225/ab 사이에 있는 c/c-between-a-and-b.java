b8016e19c7db1910dcc5fc7901e5a28f42218f19
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-25 16:43:05 +0900

    Title: ab 사이에 있는 c, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231225/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        boolean condition = false;
+        for (int i = a; i < b + 1; i++) {
+            if (i % c == 0) {
+                condition = true;
+                break;
+            }
+        }
+
+        if (condition) {
+            bw.write("YES");
+        } else {
+            bw.write("NO");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
