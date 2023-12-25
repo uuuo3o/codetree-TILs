9c43e9282cdfe1ac432796abd1bc0b9e257cf250
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-25 16:51:38 +0900

    Title: 공약수의 유무, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231225/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        boolean condition = false;
+        for (int i = a; i < b + 1; i++) {
+            if ((1920 % i == 0) || (2880 % i == 0)) {
+                condition = true;
+                break;
+            }
+        }
+
+        if (condition) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
