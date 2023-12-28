72baa867070fa964d70779d81f04ad6e626e193a
JIN <80706216+uuuo3o@users.noreply.github.com>
2023-12-28 20:52:26 +0900

    Title: 두 개의 직각삼각형, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                bw.write("*");
+            }
+            for (int j = 0; j < i * 2; j++) {
+                bw.write(" ");
+            }
+            for (int j = n; j > i; j--) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
