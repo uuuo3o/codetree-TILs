c8e7afc1019a888a5730b08bc1968d8327404545
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-01 23:34:03 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240101/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i > 0; i--) {
+            for (int j = n; j > 0; j--) {
+                bw.write(j + " ");
+            }
+            bw.write("\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
