6b0c0323b77afec07396c0e9738b5fbc34ee9f52
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-03 19:51:26 +0900

    Title: 구구단 만들기 2, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for (int i = 2; i <= 8; i += 2) {
+            for (int j = b; j >= a; j--) {
+                bw.write(j + " * " + i + " = " + (j * i));
+
+                if (j > a) {
+                    bw.write(" / ");
+                }
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
