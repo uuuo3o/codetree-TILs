2585dd8b19a10be6b4b74241178ce23b82d47b0d
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-03 19:43:20 +0900

    Title: 정상적인 구구단, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int num = 0;
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                bw.write(i + " * " + j + " = " + (i * j));
+                if (j < n) {
+                    bw.write(", ");
+                }
+            }
+
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
