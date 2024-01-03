82aa7114162fdf3bc73be6db206418325fc97bba
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-04 00:25:15 +0900

    Title: 숫자 피라미드 3, Time: 127ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240104/숫자 피라미드 3/number-pyramid-3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                bw.write((i * j) + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
