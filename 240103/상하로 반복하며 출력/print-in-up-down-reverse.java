ff1cfe161829f1e97750ba2b3b7a492036b19f2c
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-03 19:21:05 +0900

    Title: 상하로 반복하며 출력, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j % 2 == 0) {
+                    bw.write((i + 1) + "");
+                } else {
+                    bw.write((n - i) + "");
+                }
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
