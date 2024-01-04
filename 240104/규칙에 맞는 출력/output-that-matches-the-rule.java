08b39c7c9cc3aefa06f4cb88b0f081aa9f79402a
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-04 23:10:29 +0900

    Title: 규칙에 맞는 출력, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240104/규칙에 맞는 출력/output-that-matches-the-rule.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j <= n; j++) {
+                bw.write(j + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
