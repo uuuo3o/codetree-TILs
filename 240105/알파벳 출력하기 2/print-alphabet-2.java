c96d0d08caca9ff8208bbb0bba6f615e1c179f35
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-05 19:21:11 +0900

    Title: 알파벳 출력하기 2, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240105/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int askii = 65;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write("  ");
+            }
+            for (int j = i; j < n; j++) {
+                if (askii > 90) {
+                    askii = 65;
+                }
+
+                bw.write((char) askii++ + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
