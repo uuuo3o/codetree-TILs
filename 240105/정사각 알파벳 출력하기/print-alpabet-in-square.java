d1f28b9cd48d707f0d675d26d4b71baa628e346c
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-05 19:14:40 +0900

    Title: 정사각 알파벳 출력하기, Time: 139ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240105/정사각 알파벳 출력하기/print-alpabet-in-square.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int askii = 65;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write((char)askii++);
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
