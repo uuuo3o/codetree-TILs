78eecdbe78a9ba66442d94255b3557410b27707c
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-09 16:35:20 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240109/연속부분수열일까/contiguous-array-or-not.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n1 = Integer.parseInt(st.nextToken());  // 수열 A의 length
+        int n2 = Integer.parseInt(st.nextToken());  // 수열 B의 length
+
+        int[] A = new int[n1];  // 수열 A
+        int[] B = new int[n2];  // 수열 B
+
+        // 수열 A의 원소
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n1; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 수열 B의 원소
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n2; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 수열 B가 수열 A의 연속부분순열인지 판별
+        boolean result = false;
+        int idx = 0, i = 0;
+        while (i < n2) {
+            if (A[idx++] == B[i]) {
+                i++;
+                result = true;
+            } else {
+                result = false;
+
+                if (i >= 1) {
+                    break;
+                }
+            }
+        }
+
+        if (result) {
+            bw.write("Yes");
+        } else {
+            bw.write("No");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
