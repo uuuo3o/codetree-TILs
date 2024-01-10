181a9039a66b06af48bd90b7c2308c795a4812f6
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-10 23:24:49 +0900

    Title: 격자에 점 그리기, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240110/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] array = new int[n][n];
+
+        int num = 1;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            array[r - 1][c - 1] = num++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write(array[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
