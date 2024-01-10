6d8bc223e7ce969da4371bec24ba60568d1f0568
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-11 00:32:17 +0900

    Title: 두 숫자의 차의 최솟값, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240111/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        int minus = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    minus = Math.abs(array[i] - array[j]);
+                    min = Math.min(min, minus);
+                }
+            }
+        }
+
+        bw.write(min + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
