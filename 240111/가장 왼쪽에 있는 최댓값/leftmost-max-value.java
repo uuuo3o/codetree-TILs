f2c4ae3d528d4b0bf7deebdcb0d15b2038f1c3bf
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-11 00:02:03 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 261ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240111/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int idx = n;
+        while (idx != 0) {
+            int maxValue = -1;
+            for (int i = idx - 1; i >= 0; i--) {
+                if (maxValue <= array[i]) {
+                    maxValue = array[i];
+                    idx = i;
+                }
+            }
+            bw.write((idx + 1) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
