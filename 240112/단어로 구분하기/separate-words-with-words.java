38b5ac34dc363c2225ed987db2cd200ff0e21ad8
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-12 18:17:03 +0900

    Title: 단어로 구분하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240112/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = 10;
+        String[] strArr = new String[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            strArr[i] = st.nextToken();
+        }
+
+        for (int i = 0; i < n; i++) {
+            bw.write(strArr[i] + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
