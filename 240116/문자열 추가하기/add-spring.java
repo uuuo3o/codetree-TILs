45bf25fd8f7b847fa747c106c0cd297c5a05a53f
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-16 22:47:57 +0900

    Title: 문자열 추가하기, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240116/문자열 추가하기/add-spring.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String str = br.readLine();
+
+        str += "Hello";
+
+        bw.write(str);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
