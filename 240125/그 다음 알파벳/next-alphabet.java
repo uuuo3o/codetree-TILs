1bb3cb879c601eab5f33caa6c57ac8278346adbc
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-25 22:04:39 +0900

    Title: 그 다음 알파벳, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240125/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        char alphabet = st.nextToken().charAt(0);
+        int askii = (int) alphabet;
+
+        if (askii == 122) {
+            askii = 96;
+        }
+
+        bw.write(((char) askii + 1));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
