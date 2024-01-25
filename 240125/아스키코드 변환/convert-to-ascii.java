54197a66c5adb980b63c92d3d8581734148fcadb
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-25 22:01:35 +0900

    Title: 아스키코드 변환, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240125/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        char a = st.nextToken().charAt(0);
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(((int) a) + " " + ((char) b));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
