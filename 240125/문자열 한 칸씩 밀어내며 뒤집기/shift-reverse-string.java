294368270ad3a08a63b756d7db059ec9c6c0be7a
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-25 21:51:49 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 207ms, Memory: 19MB, Status: Passed - CodeTree

--- 240125/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 240125/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -16,14 +16,14 @@ public class Main {
 
             if (request == 1) {
                 A = A.substring(1) + A.charAt(0);
-                bw.write(A + "\n");
             } else if (request == 2){
                 A = A.substring(A.length() - 1) + A.substring(0, A.length() - 1);
-                bw.write(A + "\n");
             } else {
                 StringBuilder reversedA = new StringBuilder(A).reverse();
-                bw.write(reversedA + "\n");
+                A = reversedA.toString();
             }
+
+            bw.write(A + "\n");
         }
 
         bw.flush();
