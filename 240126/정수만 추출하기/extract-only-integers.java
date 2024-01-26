252f8429adaa2c844ec6a4a9c029e3c0a73116db
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-01-26 13:19:26 +0900

    Title: 정수만 추출하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240126/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+        
+        StringBuilder newA = new StringBuilder();
+        StringBuilder newB = new StringBuilder();
+
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) >= '0' && A.charAt(i) <= '9') {
+                newA.append(A.charAt(i));
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i < B.length(); i++) {
+            if (B.charAt(i) >= '0' && B.charAt(i) <= '9') {
+                newB.append(B.charAt(i));
+            } else {
+                break;
+            }
+        }
+
+        int intA = Integer.parseInt(newA.toString());
+        int intB = Integer.parseInt(newB.toString());
+        
+        bw.write((intA + intB) + "");
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
