7c38a4b3f875f99b605a3df70878e06b934f9395
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-02-23 16:20:05 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240223/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int idx = 0;
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String str = br.readLine();
+        String target = br.readLine();
+
+        findIndex(str, target);
+
+        bw.write(idx + " ");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void findIndex(String str, String target) {
+        boolean condition = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == target.charAt(0)) {
+                condition = true;
+                idx = i + 1;
+
+                for (int j = 1; j < target.length(); j++) {
+                    if (str.charAt(idx++) != target.charAt(j)) {
+                        idx = -1;
+                        condition = false;
+                        break;
+                    }
+                }
+
+                if (condition) {
+                    idx -= target.length();
+                    break;
+                }
+
+            } else {
+                idx = -1;
+            }
+        }
+    }
+}
