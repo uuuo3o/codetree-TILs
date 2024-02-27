f0aebc0c4b19ce67072bfcc411af4365a7d1e97b
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-02-27 15:07:56 +0900

    Title: Time to Time, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240227/Time to Time/time-to-time.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int startTime = a * 60 + b;
+        int endTime = c * 60 + d;
+
+        bw.write(endTime - startTime + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
