a89431e83e9728d98859b9f02ebc5c52e2384af1
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-02-29 01:10:14 +0900

    Title: 사각형의 총 넓이 2, Time: 292ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240229/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        final int OFFSET = 100;
+        int[][] matrix = new int[201][201];
+
+        int sum = 0;
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int y1 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int x2 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int y2 = Integer.parseInt(st.nextToken()) + OFFSET;
+
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
+                    if (matrix[j][k] == 1) {
+                        break;
+                    }
+                    matrix[j][k]++;
+                }
+            }
+        }
+
+        bw.write(Arrays.stream(matrix)
+                .flatMapToInt(Arrays::stream)
+                .sum() + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
