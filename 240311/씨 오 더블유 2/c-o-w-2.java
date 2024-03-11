fc01ee7e8f8e289d4fbb3c5734458d67c940b15d
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-03-11 15:13:30 +0900

    Title: 씨 오 더블유 2, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240311/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (str.charAt(i) == 'C' && str.charAt(j) == 'O' &&
+                            str.charAt(k) == 'W') {
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        bw.write(cnt + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
