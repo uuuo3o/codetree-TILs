388d894c012b95b0d44cd6ca0f5d66d2cd68e674
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-03-12 15:50:29 +0900

    Title: G or H 3, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240312/G or H 3/G-or-H-3.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] picture = new int[10001];
+
+        int maxPosition = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int position = Integer.parseInt(st.nextToken());
+            char sign = st.nextToken().charAt(0);
+
+            maxPosition = Math.max(maxPosition, position);
+
+            if (sign == 'G') {
+                picture[position] = 1;
+            } else {
+                picture[position] = 2;
+            }
+
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 1; i < maxPosition - k + 1; i++) {
+            int sum = 0;
+
+            for (int j = i; j < i + k + 1; j++) {
+                sum += picture[j];
+            }
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        bw.write(maxSum + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
