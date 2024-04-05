b3899504c13c9268c2d341b5dc762eb086042af4
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-04-05 23:56:33 +0900

    Title: 초기 수열 복원하기, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240405/초기 수열 복원하기/restore-initial-sequence.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] original = new int[n];
+        int[] sumArr = new int[n - 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < sumArr.length; i++) {
+            sumArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean isTrue = false;
+        int num = 1;
+        while (num <= n) {
+
+            int idx = 0;
+            original[0] = num++;
+
+            for (int i = 1; i <= n; i++) {
+
+                if (idx == sumArr.length) {
+                    break;
+                }
+
+                if (original[idx] != i && original[idx] + i == sumArr[idx]) {
+                    original[++idx] = i;
+                    i = 1;
+                    isTrue = true;
+                } else {
+                    isTrue = false;
+                }
+            }
+
+            if (isTrue) {
+                break;
+            }
+        }
+
+        for (int i = 0; i < original.length; i++) {
+            bw.write(original[i] + " ");
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
