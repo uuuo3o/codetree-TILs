ef1e2fafbab034adab0deffd6cb5d2d08a5713c6
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-04-20 21:53:23 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240420/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] checkPoints = new int[n][2];
+
+        for (int i = 0; i < checkPoints.length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            checkPoints[i][0] = Integer.parseInt(st.nextToken());
+            checkPoints[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int minDistance = 9999;
+        for (int i = 1; i < checkPoints.length - 1; i++) {
+
+            int distance = 0;
+            int[][] newCheckPoints = new int[n - 1][2];
+            int idx = 0;
+
+            for (int j = 0; j < checkPoints.length; j++) {
+
+                if (i == j) continue;
+
+                newCheckPoints[idx][0] = checkPoints[j][0];
+                newCheckPoints[idx++][1] = checkPoints[j][1];
+            }
+
+            for (int j = 0; j < newCheckPoints.length - 1; j++) {
+                int dx = Math.abs(newCheckPoints[j][0] - newCheckPoints[j + 1][0]);
+                int dy = Math.abs(newCheckPoints[j][1] - newCheckPoints[j + 1][1]);
+                distance += (dx + dy);
+            }
+
+            minDistance = Math.min(minDistance, distance);
+        }
+
+        bw.write(minDistance + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
