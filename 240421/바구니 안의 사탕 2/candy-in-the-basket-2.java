9b7a620c9d74cad0342ed9c6ce2755ed44e60993
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-04-21 14:41:10 +0900

    Title: 바구니 안의 사탕 2, Time: 110ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240421/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = new int[] {1, 1, 1, -1, -1, -1, 0, 0};
+    static int[] dy = new int[] {-1, 0, 1, -1, 0, 1, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][2];
+
+        int maxNumber = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            int candy = Integer.parseInt(st.nextToken());
+            int basketNum = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = candy;
+            arr[i][1] = basketNum;
+
+            maxNumber = Math.max(maxNumber, basketNum);
+        }
+
+        int[] candy = new int[maxNumber + 1];
+        for (int i = 0; i < arr.length; i++) {
+            candy[arr[i][1]] = arr[i][0];
+        }
+
+        int maxCandy = 0;
+        for (int i = k; i < candy.length - k; i++) {
+
+            int sum = 0;
+
+            for (int j = i - k; j <= i + k; j++) {
+                sum += candy[j];
+            }
+
+            maxCandy = Math.max(maxCandy, sum);
+        }
+
+        bw.write(maxCandy + "");
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
