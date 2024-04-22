7c4e1b88593d0cb7f2b9713bdf43810c36514ef6
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-04-22 15:54:30 +0900

    Title: G or H 2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240422/G or H 2/G-or-H-2.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = new int[] {1, 1, 1, -1, -1, -1, 0, 0};
+    static int[] dy = new int[] {-1, 0, 1, -1, 0, 1, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        String[] sign = new String[101];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int position = Integer.parseInt(st.nextToken());
+            String alphabet = st.nextToken();
+
+            sign[position] = alphabet;
+        }
+
+        int maxLength = 0;
+        for (int i = 0; i < sign.length; i++) {
+
+            int gCnt = 0;
+            int hCnt = 0;
+
+            if (sign[i] == null) continue;
+
+            if (sign[i].equals("G")) gCnt++;
+            else hCnt++;
+
+            for (int j = i + 1; j < sign.length; j++) {
+                
+                if (sign[j] == null) continue;
+
+                if (sign[j].equals("G")) gCnt++;
+                else hCnt++;
+
+                if (gCnt == hCnt) {
+                    maxLength = Math.max(maxLength, j - i);
+                }
+            }
+
+
+        }
+
+        bw.write(maxLength + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
