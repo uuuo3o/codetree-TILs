60e2faea3a3fd1b12c0129187f3f241ac5094ef3
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-04-23 16:03:30 +0900

    Title: 숫자 카운트, Time: 102ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240423/숫자 카운트/numeric-count.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = new int[] {1, 1, 1, -1, -1, -1, 0, 0};
+    static int[] dy = new int[] {-1, 0, 1, -1, 0, 1, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] numArr = new int[n];
+        int[][] ballCnt = new int[n][2];
+
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            numArr[i] = Integer.parseInt(st.nextToken());
+            ballCnt[i][0] = Integer.parseInt(st.nextToken());
+            ballCnt[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                for (int k = 1; k <= 9; k++) {
+                    
+                    boolean isSatisfied = true;
+
+                    for (int l = 0; l < n; l++) {
+
+                        String num = String.valueOf(numArr[l]);
+
+                        int first = Integer.parseInt(String.valueOf(num.charAt(0)));
+                        int second = Integer.parseInt(String.valueOf(num.charAt(1)));
+                        int third = Integer.parseInt(String.valueOf(num.charAt(2)));
+
+                        int sCnt = 0;
+                        int bCnt = 0;
+
+                        if (first == i) sCnt++;
+                        if (second == j) sCnt++;
+                        if (third == k) sCnt++;
+
+                        if (second == i || third == i) bCnt++;
+                        if (first == j || third == j) bCnt++;
+                        if (first == k || second == k) bCnt++;
+
+                        if (!(sCnt == ballCnt[l][0] && bCnt == ballCnt[l][1])) {
+                            isSatisfied = false;
+                            break;
+                        }
+                    }
+
+                    if (isSatisfied) result++;
+                }
+            }
+        }
+
+        bw.write(result + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
