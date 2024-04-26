e5031ec2f388b847a0bb2e8c84e943a91cb82f84
JIN <80706216+uuuo3o@users.noreply.github.com>
2024-04-26 21:41:50 +0900

    Title: 정보에 따른 숫자 2, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240426/정보에 따른 숫자 2/number-based-on-information-2.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());   // 알파벳의 수
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        String[] line = new String[1001];
+        for (int i = 0; i < T; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            String alphabet = st.nextToken();
+            int distance = Integer.parseInt(st.nextToken());
+
+            line[distance] = alphabet;
+        }
+
+        int result = 0;
+        for (int i = a; i <= b; i++) {
+
+            int minD1 = 1000;
+            int minD2 = 1000;
+            for (int j = 1; j <= b; j++) {
+
+                if (line[j] == null) continue;
+
+                if (line[j].equals("S")) {
+                    minD1 = Math.min(minD1, Math.abs(i - j));
+                }
+                if (line[j].equals("N")) {
+                    minD2 = Math.min(minD2, Math.abs(i - j));
+                }
+            }
+
+            if (minD1 <= minD2) {
+                result++;
+            }
+        }
+
+        bw.write(result + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
